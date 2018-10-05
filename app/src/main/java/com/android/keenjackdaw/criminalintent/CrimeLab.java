613275c918b325894908ece3cfe8cf1da82aa121
Keen Jackdaw <keenjackdaw@gmail.com>
2018-10-05 17:35:04 +0800

    add fun in CrimeLab

--- app/src/main/java/com/android/keenjackdaw/criminalintent/CrimeLab.java
+++ app/src/main/java/com/android/keenjackdaw/criminalintent/CrimeLab.java
@@ -2,9 +2,14 @@ package com.android.keenjackdaw.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 // Singleton
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
@@ -13,5 +18,21 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
-    private CrimeLab(Context context) { }
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    public List<Crime> getmCrimes(){
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+
+        for(Crime crime: mCrimes){
+            if(crime.getmId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
 }
